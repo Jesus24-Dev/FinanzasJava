edb719e0abef4c79a88dfb2d0af35fd3901c6496
Jesus24-Dev <siritjesus24@gmail.com>
2024-10-02 18:26:39 -0400

    consulta insert para guardar gasto e ingreso

--- src/main/java/finanzasapp/modelos/Gasto.java
+++ src/main/java/finanzasapp/modelos/Gasto.java
@@ -1,6 +1,11 @@
 
 package finanzasapp.modelos;
 
+import finanzasapp.modelos.conexion.Conexion;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class Gasto {
     private int id_gasto;
     private int id_cuenta;
@@ -15,38 +20,29 @@ public class Gasto {
         this.descripcion = descripcion;
         this.monto = monto;
     }
-
-    public int getId_gasto() {
-        return id_gasto;
-    }
-
-    public int getId_cuenta() {
-        return id_cuenta;
-    }
-
-    public String getFecha() {
-        return fecha;
-    }
-
-    public String getDescripcion() {
-        return descripcion;
-    }
-
-    public double getMonto() {
-        return monto;
-    }
-
-    public void setFecha(String fecha) {
-        this.fecha = fecha;
-    }
-
-    public void setDescripcion(String descripcion) {
-        this.descripcion = descripcion;
-    }
-
-    public void setMonto(double monto) {
-        this.monto = monto;
-    }
     
+    public static void guardarGasto(String dia, String mes, String anio, String descripcion, double monto){
+        String sql1 = "INSERT INTO gasto (id_cuenta, dia, mes, anio, descripcion, monto) VALUES (?, ?, ?, ?, ?, ?)";
+        String sql2 = "UPDATE cuenta SET saldo_actual = saldo_actual - ?";
+        try (Connection conn = Conexion.connect();
+            PreparedStatement stmt = conn.prepareStatement(sql1);
+            PreparedStatement stmt2 = conn.prepareStatement(sql2)){ 
+            conn.setAutoCommit(false);
+            stmt.setInt(1, 1);
+            stmt.setString(2, dia);
+            stmt.setString(3, mes);
+            stmt.setString(4, anio);
+            stmt.setString(5, descripcion);
+            stmt.setDouble(6, monto);
+            
+            
+            stmt2.setDouble(1, monto);
+            stmt.executeUpdate();
+            stmt2.executeUpdate();
+            conn.commit();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
     
 }
--- src/main/java/finanzasapp/modelos/Ingreso.java
+++ src/main/java/finanzasapp/modelos/Ingreso.java
@@ -1,52 +1,58 @@
 
 package finanzasapp.modelos;
 
+import finanzasapp.modelos.conexion.Conexion;
+import java.sql.*;
+
 public class Ingreso {
+
     private int id_ingreso;
     private int id_cuenta;
     private String fecha;
     private String descripcion;
     private double monto;
-
-    public Ingreso(int id_ingreso, int id_cuenta, String fecha, String descripcion, double monto) {
-        this.id_ingreso = id_ingreso;
-        this.id_cuenta = id_cuenta;
-        this.fecha = fecha;
-        this.descripcion = descripcion;
-        this.monto = monto;
-    }
-
-    public int getId_ingreso() {
-        return id_ingreso;
-    }
-
-    public int getId_cuenta() {
-        return id_cuenta;
-    }
-
-    public String getFecha() {
-        return fecha;
-    }
-
-    public String getDescripcion() {
-        return descripcion;
-    }
-
-    public double getMonto() {
-        return monto;
-    }
-
-    public void setFecha(String fecha) {
-        this.fecha = fecha;
-    }
-
-    public void setDescripcion(String descripcion) {
-        this.descripcion = descripcion;
-    }
-
-    public void setMonto(double monto) {
-        this.monto = monto;
-    }
     
-    
+    public static void guardarIngreso(String dia, String mes, String anio, String descripcion, double monto){
+        String sql1 = "INSERT INTO ingreso (id_cuenta, dia, mes, anio, descripcion, monto) VALUES (?, ?, ?, ?, ?, ?)";
+        String sql2 = "UPDATE cuenta SET saldo_actual = saldo_actual + ?";
+        try (Connection conn = Conexion.connect();
+            PreparedStatement stmt = conn.prepareStatement(sql1);
+            PreparedStatement stmt2 = conn.prepareStatement(sql2)){ 
+            conn.setAutoCommit(false);
+            stmt.setInt(1, 1);
+            stmt.setString(2, dia);
+            stmt.setString(3, mes);
+            stmt.setString(4, anio);
+            stmt.setString(5, descripcion);
+            stmt.setDouble(6, monto);
+            
+            
+            stmt2.setDouble(1, monto);
+            stmt.executeUpdate();
+            stmt2.executeUpdate();
+            conn.commit();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+  
+    public static void verIngresos(){
+        String sql = "SELECT fecha, descripcion, monto FROM ingreso WHERE id_cuenta = ?";
+        try (Connection conn = Conexion.connect();
+            Statement stmt = conn.createStatement();
+            PreparedStatement psmt = conn.prepareStatement(sql) ){     
+            psmt.setInt(1, 1);
+            ResultSet rs = psmt.executeQuery();
+            while(rs.next()){
+                
+                String fecha = rs.getString("fecha");
+                String descripcion = rs.getString("descripcion");
+                double monto = rs.getDouble("monto");
+                
+                System.out.println("Fecha: " + fecha + "\nDescripcion: " + descripcion + "\nMonto: " + monto);
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
