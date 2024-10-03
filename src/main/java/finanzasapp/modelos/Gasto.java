e00bdd8b883b722e2211c25562652a23a74af8b9
Jesus24-Dev <siritjesus24@gmail.com>
2024-10-02 20:39:21 -0400

    query para obtener datos agregada

--- src/main/java/finanzasapp/modelos/Gasto.java
+++ src/main/java/finanzasapp/modelos/Gasto.java
@@ -4,7 +4,10 @@ package finanzasapp.modelos;
 import finanzasapp.modelos.conexion.Conexion;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Gasto {
     private int id_gasto;
@@ -43,6 +46,49 @@ public class Gasto {
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
+    }
+    
+    public static Object[][] verGastos(String mm, String yyyy) {
+       List<Object> ingresos = new ArrayList<>();
+       String sql = "SELECT dia, mes, anio, monto, descripcion, " +
+               "CASE mes " +
+               "WHEN 'Enero' THEN 1 " +
+               "WHEN 'Febrero' THEN 2 " +
+               "WHEN 'Marzo' THEN 3 " +
+               "WHEN 'Abril' THEN 4 " +
+               "WHEN 'Mayo' THEN 5 " +
+               "WHEN 'Junio' THEN 6 " +
+               "WHEN 'Julio' THEN 7 " +
+               "WHEN 'Agosto' THEN 8 " +
+               "WHEN 'Septiembre' THEN 9 " +
+               "WHEN 'Octubre' THEN 10 " +
+               "WHEN 'Noviembre' THEN 11 " +
+               "WHEN 'Diciembre' THEN 12 " +
+               "END AS mes_numero " +
+               "FROM gasto " +
+               "WHERE mes = ? AND anio = ? " +
+               "ORDER BY anio DESC, mes_numero DESC, dia DESC;";
+
+        try (Connection conn = Conexion.connect();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setString(1, mm);
+            stmt.setString(2, yyyy);
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                int dia = rs.getInt("dia");
+                String mes = rs.getString("mes");
+                int anio = rs.getInt("anio");
+                double monto = rs.getDouble("monto");
+                String descripcion = rs.getString("descripcion");
+                String fecha = dia + "-" + mes + "-" + anio; 
+                ingresos.add(new Object[]{monto, descripcion, fecha});
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        Object[][] movimientosArray = new Object[ingresos.size()][];
+        movimientosArray = ingresos.toArray(movimientosArray); 
+        return movimientosArray;
     }
     
 }
