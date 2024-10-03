3f7501bef729673b3bfea17dddc22e89348df785
Jesus24-Dev <siritjesus24@gmail.com>
2024-10-03 11:46:46 -0400

    archivo main de la app

--- src/main/java/finanzasapp/FinanzasApp.java
+++ src/main/java/finanzasapp/FinanzasApp.java
@@ -2,10 +2,16 @@
 
 package finanzasapp;
 
+import com.gui.Principal;
+import com.gui.Window;
+import finanzasapp.modelos.Cuenta;
+
 
 public class FinanzasApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+        Window win = new Window();    
+        win.setVisible(true);
+        win.setLocationRelativeTo(null); 
+    }    
 }
